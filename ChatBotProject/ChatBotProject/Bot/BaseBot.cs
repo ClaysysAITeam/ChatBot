63417984863e774490439bbefe90078fe2d44473
PaxstanFernandez <paxstan.claysys@gmail.com>
2019-09-18 15:46:26 +0530

    Message router fixed

--- ChatBotProject/ChatBotProject/Bot/BaseBot.cs
+++ ChatBotProject/ChatBotProject/Bot/BaseBot.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ChatBotProject.Bot
 {
-	public class BaseBot
-	{
-	}
+	public class BaseBot { }
 }
